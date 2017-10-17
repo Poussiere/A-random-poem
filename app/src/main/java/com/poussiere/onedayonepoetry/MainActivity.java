284ac8a150ceb629800db6775ee413b77fb79a6d
poussiere <von.linnasta@gmail.com>
2017-10-17 22:23:26 +0200

    init

--- app/src/main/java/com/poussiere/onedayonepoetry/MainActivity.java
+++ app/src/main/java/com/poussiere/onedayonepoetry/MainActivity.java
@@ -3,11 +3,39 @@ package com.poussiere.onedayonepoetry;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.poussiere.onedayonepoetry.PoetryRequest.BASE_URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        PoetryRequest poetryRequest = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(PoetryRequest.class);
+
+        mCompositeDisposable.add(poetryRequest.register()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse,this::handleError));
+
     }
+
+
 }
